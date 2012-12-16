026d63b030c8d2b486b08e875b85b69178ca1bc0
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2012-12-16 18:02:35 +1100

    streamlined rendering of templates

---
+++ html/template.go
@@ -0,0 +1,25 @@
+package html
+
+import "bytes"
+import "io/ioutil"
+import "html/template"
+
+func Template(name string) string{
+	content, err := ioutil.ReadFile("template/"+name)
+	if err != nil { panic(err) }
+	return string(content)
+}
+
+func RenderString(content string, data interface{}) string {
+	//TODO be creative with name
+	tmpl, err := template.New("").Parse(content)
+	if err != nil { panic(err) }
+	var doc bytes.Buffer
+	err = tmpl.Execute(&doc, data)
+	if err != nil { panic(err) }
+	return doc.String()
+}
+
+func RenderTemplate(name string, data interface{}) string{
+	return RenderString(Template(name),data)
+}
