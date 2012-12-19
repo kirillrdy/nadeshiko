5018c8ecd0121a90d1b0bdcf65db171a8e9c9899
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2012-12-20 08:42:55 +1100

    nadeshiko now terminates goroutines that try to send message on dead socket this is done for cleaning up timers

--- connection.go
+++ connection.go
@@ -2,6 +2,7 @@ package nadeshiko
 
 import "code.google.com/p/go.net/websocket"
 import "fmt"
+import "runtime"
 
 type WebsocketConnection websocket.Conn
 
@@ -12,35 +13,13 @@ type Connection struct {
 
 func (connection *Connection) SendMessage(message string) {
 	real_websocket := websocket.Conn(*connection.websocket)
-	websocket.Message.Send(&real_websocket, message)
+	err := websocket.Message.Send(&real_websocket, message)
 
+	// This is done to cleanup timers that are not terminated
+	// but will try to send on close sockets
+	// perhaps we can do this when we change activities
+	if err != nil { runtime.Goexit() }
 
-	// TODO perhaps good to keep, on Send cleanup sockets
-	// however most of those should be caught in the main loop
-	//err := websocket.Message.Send(&real_websocket, message)
-
-	//if err != nil {
-	//	for k, v := range Notifications {
-	//		var new_list []JFunc
-	//		for _, jFunction := range v {
-	//			if jFunction("").ws != ws {
-	//				new_list = append(new_list,jFunction)
-	//			}
-	//		}
-	//		Notifications[k] = new_list
-	//	}
-
-	//	for callback_id, callbackStruct := range Callbacks {
-	//		if callbackStruct.ws == ws {
-	//			delete(Callbacks, callback_id)
-	//			fmt.Printf("Removing callback %s for disconnected client\n",callback_id)
-	//		}
-	//	}
-	//}
-
-
-
-	//TODO if we fail send we should remove Callbacks and Notifications
 	if Verbose {
 		fmt.Printf("send: %s\n", message)
 	}
