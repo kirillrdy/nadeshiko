6a8c6a127032c7afd08e4bd1dab7cc04d5cafc3b
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2013-01-09 20:44:25 +1100

    just moved prints in right places

--- nadeshiko.go
+++ nadeshiko.go
@@ -7,25 +7,22 @@ import (
 	"fmt"
 )
 
-//TODO consider getting rid of this
 var DefaultActivity Activity
 
 func Start(activity Activity, port int, verbose bool) {
 
 	DefaultActivity = activity
-
-
 	Verbose = verbose
 
+	log.Println("Starting Nadeshiko Server " + NADESHIKO_VERSION)
 	http.Handle("/websocket_client", websocket.Handler(websocketServer))
 	http.HandleFunc("/", fileServer)
-
-	log.Println("Started Nadeshiko Server " + NADESHIKO_VERSION)
-	log.Printf("Listening http://localhost:%d/\n", port)
 
 	listenOn := fmt.Sprintf(":%d", port)
 	err := http.ListenAndServe(listenOn, nil)
 	if err != nil {
 		log.Fatalln("ListenAndServe: " + err.Error())
 	}
+
+	log.Printf("Listening http://localhost:%d/\n", port)
 }
