d0d9657d8bc5407475654a88486a6fa57aef53e0
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2013-10-20 12:58:55 +1100

    Less verbose

--- file_server.go
+++ file_server.go
@@ -9,15 +9,20 @@ import (
 var Verbose bool
 
 func fileServer(w http.ResponseWriter, req *http.Request) {
-	log.Printf("=======================================================\n")
+	if Verbose {
+		log.Printf("=======================================================\n")
+	}
+
 	requested_path := req.RequestURI
 
 	if requested_path == "/" {
 		requested_path = "/index.html"
 	}
 
-	log.Printf("GET: %q \n", requested_path)
-	log.Printf("User-Agent: %s \n\n", req.Header["User-Agent"])
+	log.Printf("HTTP GET: %q \n", requested_path)
+	if Verbose {
+		log.Printf("User-Agent: %s \n\n", req.Header["User-Agent"])
+	}
 
 	w.Header().Set("Server", "Nadeshiko " + NADESHIKO_VERSION)
 
