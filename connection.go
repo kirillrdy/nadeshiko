254015e0fd53b074194d72e58a2da6a6b7ec017f
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-02-03 09:15:08 +1100

    Mutex !!!

--- connection.go
+++ connection.go
@@ -4,17 +4,23 @@ import (
 	"code.google.com/p/go.net/websocket"
 	"log"
 	"runtime"
+	"sync"
 )
 
 type WebsocketConnection websocket.Conn
 
 type Connection struct {
 	websocket       *WebsocketConnection
+	mutex			sync.Mutex
 }
 
 func (connection *Connection) SendMessage(message string) {
+
+	connection.mutex.Lock()
 	real_websocket := websocket.Conn(*connection.websocket)
 	err := websocket.Message.Send(&real_websocket, message)
+
+	connection.mutex.Unlock()
 
 	// This is done to cleanup timers that are not terminated
 	// but will try to send on close sockets
