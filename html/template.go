5f5e9564d82e7a64aa90246cf1e898ecddc50151
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-03-10 16:17:01 +1100

    rethinking how we do templates

--- html/template.go
+++ html/template.go
@@ -1,33 +1,35 @@
 package html
 
-import (
-	"bytes"
-	"io/ioutil"
-	"html/template"
-	"runtime"
-	"path"
-)
-
-func LoadTemplate(name string) string{
-	_, caller_file_name, _, _ := runtime.Caller(1)
-	dir := path.Dir(caller_file_name)
-
-	path := path.Join(dir,"template/", name)
-	content, err := ioutil.ReadFile(path)
-	if err != nil { panic(err) }
-	return string(content)
-}
-
-func RenderString(content string, data interface{}) string {
-	//TODO be creative with name
-	tmpl, err := template.New("").Parse(content)
-	if err != nil { panic(err) }
-	var doc bytes.Buffer
-	err = tmpl.Execute(&doc, data)
-	if err != nil { panic(err) }
-	return doc.String()
-}
-
-func RenderTemplate(name string, data interface{}) string{
-	return RenderString(LoadTemplate(name),data)
-}
+//I'm reconsidering if its better to deal with this in go way
+//
+//import (
+//	"bytes"
+//	"io/ioutil"
+//	"html/template"
+//	"runtime"
+//	"path"
+//)
+//
+//func LoadTemplate(name string) string{
+//	_, caller_file_name, _, _ := runtime.Caller(1)
+//	dir := path.Dir(caller_file_name)
+//
+//	path := path.Join(dir,"template/", name)
+//	content, err := ioutil.ReadFile(path)
+//	if err != nil { panic(err) }
+//	return string(content)
+//}
+//
+//func RenderString(content string, data interface{}) string {
+//	//TODO be creative with name
+//	tmpl, err := template.New("").Parse(content)
+//	if err != nil { panic(err) }
+//	var doc bytes.Buffer
+//	err = tmpl.Execute(&doc, data)
+//	if err != nil { panic(err) }
+//	return doc.String()
+//}
+//
+//func RenderTemplate(name string, data interface{}) string{
+//	return RenderString(LoadTemplate(name),data)
+//}
