b04d5df8e700f46efc6b93db399ea2c298cf2110
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-05-09 11:22:36 +1000

    Activity wrapper for routes

--- routes.go
+++ routes.go
@@ -1,5 +1,7 @@
 package nadeshiko
 
+import "net/http"
+
 const GET = "GET"
 const POST = "POST"
 
@@ -12,5 +14,18 @@ func (routes *Routes) Get(path string, handler HttpHander) {
 
 func (routes *Routes) Post(path string, handler HttpHander) {
 	route := Route{path, POST, handler}
+	*routes = append(*routes, route)
+}
+
+func (routes *Routes) Activity(path string, activity Activity) {
+	// need to compose our own handler that servers index.html for nadeshiko
+	handler := func(response http.ResponseWriter, request *http.Request) {
+		http.ServeFile(response, request, nadeshikoPublicDir()+"/index.html")
+	}
+
+	//TODO hacky, we should get rid of concept of default activity
+	DefaultActivity = activity
+
+	route := Route{path, GET, handler}
 	*routes = append(*routes, route)
 }
