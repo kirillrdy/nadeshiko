a4f79893fc317037e04a3d55a54f2ccaa44b7d2e
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-05-09 11:27:52 +1000

    Note for future

--- activity.go
+++ activity.go
@@ -5,7 +5,9 @@ type Activity interface {
 }
 
 func (connection *Connection) StartActivity(activity Activity) {
+	//TODO consider not doing this on Start
 	CleanupEventHandlers <- connection
+
 	connection.JQuery("body").Empty()
 	activity.Start(connection)
 }
