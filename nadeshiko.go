487c4886fcaa3773a956201ba9cda90b06a2de15
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-08-28 19:47:35 +1000

    Use defaultRoutes everywhere

--- nadeshiko.go
+++ nadeshiko.go
@@ -12,38 +12,32 @@ const NADESHIKO_VERSION = "0.1.0"
 
 var DefaultActivity Activity
 
-func startWithPortVerbose(routes Routes, port int, verbose bool) {
+func startWithPortVerbose(port int, verbose bool) {
 	//DefaultActivity = activity
 	Verbose = verbose
 
 	log.Println("Starting Nadeshiko Server " + NADESHIKO_VERSION)
 
 	//TODO hack, to restore our websocket handler
-	routes.Get("/nadeshiko_socket", websocket.Handler(websocketServer).ServeHTTP)
+	defaultRoutes.Get("/nadeshiko_socket", websocket.Handler(websocketServer).ServeHTTP)
 
 	listenOn := fmt.Sprintf(":%d", port)
 
 	log.Printf("Listening http://0.0.0.0:%d/\n", port)
-	err := http.ListenAndServe(listenOn, httpHandler{routes: routes})
+	err := http.ListenAndServe(listenOn, httpHandler{routes: defaultRoutes})
 	if err != nil {
 		log.Fatalln("ListenAndServe: " + err.Error())
 	}
 }
 
-func StartWithPortVerbose(routes Routes, port int) {
-	startWithPortVerbose(routes, port, true)
+func StartWithPortVerbose(port int) {
+	startWithPortVerbose(port, true)
 }
 
-func StartWithPort(routes Routes, port int) {
-	startWithPortVerbose(routes, port, false)
+func StartWithPort(port int) {
+	startWithPortVerbose(port, false)
 }
 
-func Start(routes Routes) {
-	startWithPortVerbose(routes, 3000, false)
-}
-
-func StartActivity(activity Activity) {
-	routes := Routes{}
-	routes.Activity("/", activity)
-	startWithPortVerbose(routes, 3000, false)
+func Start() {
+	startWithPortVerbose(3000, false)
 }
