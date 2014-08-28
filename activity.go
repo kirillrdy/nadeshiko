6a8ead3e289f5a5dcf3022bfdc15d1593e9a9f18
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-08-28 19:47:54 +1000

    Shortcut for starting activity

--- activity.go
+++ activity.go
@@ -11,3 +11,8 @@ func (connection *Connection) StartActivity(activity Activity) {
 	connection.JQuery("body").Empty()
 	activity.Start(connection)
 }
+
+func StartActivity(activity Activity) {
+	defaultRoutes.Activity("/", activity)
+	startWithPortVerbose(3000, false)
+}
