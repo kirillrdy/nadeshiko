f71044252172f13cd4dffbb9ecaf925c7365f69d
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-09-18 18:04:03 +1000

    Document should always be passed by reference

--- bootstrap.go
+++ bootstrap.go
@@ -1,10 +1,10 @@
 package nadeshiko
 
-func (document Document) IncludeBootstrapCdn() {
+func (document *Document) IncludeBootstrapCdn() {
 	document.JQuery("body").Append(`<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">`)
 }
 
-func (document Document) IncludeBootstrapJsCdn() {
+func (document *Document) IncludeBootstrapJsCdn() {
 	document.JQuery("body").Append(`<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>`)
 }
 
