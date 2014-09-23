60120fd6b562c09212c3ab4622f65a79c93aa2f3
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-09-23 10:16:56 +1000

    Added Attribute helper methods

---
+++ html/attributes.go
@@ -0,0 +1,25 @@
+package html
+
+func (node Node) Src(value string) Node {
+	return node.Attribute("src", value)
+}
+
+func (node Node) Style(value string) Node {
+	return node.Attribute("src", value)
+}
+
+func (node Node) Id(value string) Node {
+	return node.Attribute("id", value)
+}
+
+func (node Node) Class(value string) Node {
+	return node.Attribute("class", value)
+}
+
+func (node Node) Type(value string) Node {
+	return node.Attribute("type", value)
+}
+
+func (node Node) Value(value string) Node {
+	return node.Attribute("value", value)
+}
