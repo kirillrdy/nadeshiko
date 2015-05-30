21a2e86af15be8920d43b79beecd76acedc6f668
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-05-30 11:47:46 +1000

    Added page

---
+++ page.go
@@ -0,0 +1,17 @@
+package nadeshiko
+
+import (
+	"io"
+
+	"github.com/kirillrdy/nadeshiko/html"
+)
+
+//This is the default blank html page. that contains required clientside js code to have nadeshiko working
+func Page(path string) io.WriterTo {
+	return html.Html().Children(
+		html.Head().Children(
+			Scripts(path)...,
+		),
+		html.Body(),
+	)
+}
